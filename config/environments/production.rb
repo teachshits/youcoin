8f9a3ca9d1a3a423465046d2a933b7fcc91054c5
Mikahil Gerasimov <GerasimovMN@khv.dv.rt.ru>
2013-03-05 14:52:43 +1100

    hints

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Youcoin::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   
 end
